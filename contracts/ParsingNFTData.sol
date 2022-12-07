5d3c292da7f3a2a9c87b96f14f70875f4989090e
TheGreatHB <hb@thegreathb.com>
2022-12-07 19:29:49 +0900

    Optimize Gas of "getERC721HolderList"

--- contracts/ParsingNFTData.sol
+++ contracts/ParsingNFTData.sol
@@ -6,19 +6,25 @@ import "./IParsingNFTData.sol";
 contract ParsingNFTData is IParsingNFTData {
     function getERC721HolderList(
         IERC721 nft,
-        uint256[] calldata tokenIds
+        uint256[] calldata 
     ) external view returns (address[] memory holders) {
-        uint256 length = tokenIds.length;
-        holders = new address[](length);
+        bytes4 selector = IERC721.ownerOf.selector;
+        assembly {
+            let len := calldataload(68)
+            holders := mload(0x40)
+            mstore(holders, len)
+            let totalbytes := mul(len, 0x20)
+            mstore(0x40, add(add(holders, 0x20), totalbytes))
+            let end := add(totalbytes, 0x20)
 
-        for (uint256 i; i < length; ) {
-            try nft.ownerOf(tokenIds[i]) returns (address owner) {
-                holders[i] = owner;
-            } catch {
-                holders[i] = address(0);
-            }
-            unchecked {
-                i++;
+            // prettier-ignore
+            for { let i := 0x20 } lt(i, end) { i := add(i, 0x20) } {
+                mstore(0, selector)
+                calldatacopy(4, add(0x44, i), 0x20)
+
+                if staticcall(gas(), nft, 0, 0x24, 0, 0x20) {
+                    mstore(add(holders, i), mload(0))
+                }
             }
         }
     }
