4e6366f0d3845402c29dc73a7374f816303049ae
TheGreatHB <hb@thegreathb.com>
2022-12-11 03:06:22 +0900

    Ad functions and update getERC1155Balance

--- test/ParsingNFTData.test.ts
+++ test/ParsingNFTData.test.ts
@@ -23,43 +23,120 @@ describe("Test", function () {
         expect(res[2]).to.be.equal(user1);
         expect(res[3]).to.be.equal(user4);
     });
-    it("getERC721BalanceList", async function () {
-        const token721 = await (await ethers.getContractFactory("MockERC721")).deploy();
-        const p = await (await ethers.getContractFactory("ParsingNFTData")).deploy();
+    context("OneToken", () => {
+        it("getERC20BalanceList_OneToken", async function () {
+            const token20 = await (await ethers.getContractFactory("MockERC20")).deploy();
+            const p = await (await ethers.getContractFactory("ParsingNFTData")).deploy();
 
-        await token721.mint(user1, 1);
-        await token721.mint(user1, 2);
-        await token721.mint(user1, 3);
-        await token721.mint(user2, 4);
+            await token20.mint(user1, 10);
+            await token20.mint(user1, 20);
+            await token20.mint(user2, 30);
+            await token20.mint(user3, 40);
 
-        const res = await p.getERC721BalanceList(token721.address, [user1, user2]);
-        // console.log(res);
-        expect(res[0]).to.be.equal(3);
-        expect(res[1]).to.be.equal(1);
+            const res = await p.getERC20BalanceList_OneToken(token20.address, [user1, user2, user3, user4]);
+            // console.log(res);
+            expect(res[0]).to.be.equal(30);
+            expect(res[1]).to.be.equal(30);
+            expect(res[2]).to.be.equal(40);
+            expect(res[3]).to.be.equal(0);
+        });
+        it("getERC721BalanceList_OneToken", async function () {
+            const token721 = await (await ethers.getContractFactory("MockERC721")).deploy();
+            const p = await (await ethers.getContractFactory("ParsingNFTData")).deploy();
+
+            await token721.mint(user1, 1);
+            await token721.mint(user1, 2);
+            await token721.mint(user1, 3);
+            await token721.mint(user2, 4);
+
+            const res = await p.getERC721BalanceList_OneToken(token721.address, [user1, user2, user3]);
+            // console.log(res);
+            expect(res[0]).to.be.equal(3);
+            expect(res[1]).to.be.equal(1);
+            expect(res[2]).to.be.equal(0);
+        });
+        it("getERC1155BalanceList_OneToken", async function () {
+            const token1155 = await (await ethers.getContractFactory("MockERC1155")).deploy();
+            const p = await (await ethers.getContractFactory("ParsingNFTData")).deploy();
+
+            await token1155.mintBatch(user1, [1, 3, 5, 7, 9], [11, 13, 15, 17, 19]);
+
+            const res = await p.getERC1155BalanceList_OneToken(
+                token1155.address,
+                [user1, user2],
+                [
+                    ["1", "2", "3"],
+                    ["2", "4", "5"],
+                ]
+            );
+            // console.log(res);
+            expect(res[0][0]).to.be.equal(11);
+            expect(res[0][1]).to.be.equal(0);
+            expect(res[0][2]).to.be.equal(13);
+            expect(res[1][0]).to.be.equal(0);
+            expect(res[1][1]).to.be.equal(0);
+            expect(res[1][2]).to.be.equal(0);
+        });
     });
-    it("getERC1155BalanceList", async function () {
-        const token1155 = await (await ethers.getContractFactory("MockERC1155")).deploy();
-        const p = await (await ethers.getContractFactory("ParsingNFTData")).deploy();
+    context("OneHolder", () => {
+        it("getERC20BalanceList_OneHolder", async function () {
+            const token20_0 = await (await ethers.getContractFactory("MockERC20")).deploy();
+            const token20_1 = await (await ethers.getContractFactory("MockERC20")).deploy();
+            const token20_2 = await (await ethers.getContractFactory("MockERC20")).deploy();
+            const p = await (await ethers.getContractFactory("ParsingNFTData")).deploy();
 
-        await token1155.mintBatch(user1, [1, 3, 5, 7, 9], [11, 13, 15, 17, 19]);
+            await token20_0.mint(user1, 10);
+            await token20_1.mint(user1, 40);
 
-        const res = await p.getERC1155BalanceList(
-            token1155.address,
-            [
-                [user1, user1, user1],
-                [user2, user2, user2],
-            ],
-            [
-                ["1", "2", "3"],
-                ["2", "4", "5"],
-            ]
-        );
-        // console.log(res);
-        expect(res[0][0]).to.be.equal(11);
-        expect(res[0][1]).to.be.equal(0);
-        expect(res[0][2]).to.be.equal(13);
-        expect(res[1][0]).to.be.equal(0);
-        expect(res[1][1]).to.be.equal(0);
-        expect(res[1][2]).to.be.equal(0);
+            const res = await p.getERC20BalanceList_OneHolder(user1, [token20_0.address, token20_1.address, token20_2.address]);
+            // console.log(res);
+            expect(res[0]).to.be.equal(10);
+            expect(res[1]).to.be.equal(40);
+            expect(res[2]).to.be.equal(0);
+        });
+        it("getERC721BalanceList_OneHolder", async function () {
+            const token721_0 = await (await ethers.getContractFactory("MockERC721")).deploy();
+            const token721_1 = await (await ethers.getContractFactory("MockERC721")).deploy();
+            const token721_2 = await (await ethers.getContractFactory("MockERC721")).deploy();
+            const p = await (await ethers.getContractFactory("ParsingNFTData")).deploy();
+
+            await token721_0.mint(user1, 1);
+            await token721_0.mint(user1, 2);
+            await token721_1.mint(user1, 3);
+
+            const res = await p.getERC721BalanceList_OneHolder(user1, [token721_0.address, token721_1.address, token721_2.address]);
+            // console.log(res);
+            expect(res[0]).to.be.equal(2);
+            expect(res[1]).to.be.equal(1);
+            expect(res[2]).to.be.equal(0);
+        });
+        it("getERC1155BalanceList_OneHolder", async function () {
+            const token1155_0 = await (await ethers.getContractFactory("MockERC1155")).deploy();
+            const token1155_1 = await (await ethers.getContractFactory("MockERC1155")).deploy();
+            const token1155_2 = await (await ethers.getContractFactory("MockERC1155")).deploy();
+            const p = await (await ethers.getContractFactory("ParsingNFTData")).deploy();
+
+            await token1155_0.mintBatch(user1, [1, 3], [11, 13]);
+            await token1155_1.mintBatch(user1, [5], [15]);
+
+            const res = await p.getERC1155BalanceList_OneHolder(
+                user1,
+                [token1155_0.address, token1155_1.address, token1155_2.address],
+                [
+                    ["1", "2", "3"],
+                    ["2", "5"],
+                    ["2", "4", "5"],
+                ]
+            );
+            // console.log(res);
+            expect(res[0][0]).to.be.equal(11);
+            expect(res[0][1]).to.be.equal(0);
+            expect(res[0][2]).to.be.equal(13);
+            expect(res[1][0]).to.be.equal(0);
+            expect(res[1][1]).to.be.equal(15);
+            expect(res[2][0]).to.be.equal(0);
+            expect(res[2][1]).to.be.equal(0);
+            expect(res[2][2]).to.be.equal(0);
+        });
     });
 });
